9e9504a4b110d88e96133d25d9ff527b37e0c31b
lucasMickelsson <lucas.mickelsson@ha.ax>
2024-04-08 12:28:54 +0200

    centered the activityindicator

--- components/membersscreen.js
+++ components/membersscreen.js
@@ -51,7 +51,7 @@ const MembersScreen = () => {
 
   if (isLoading) {
     return (
-      <View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <ActivityIndicator size="large" />
       </View>
     );
